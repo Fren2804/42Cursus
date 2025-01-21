f1330967f8ed005375cd02deadfe43cae957e1db
franmore <franmore@student.42madrid.com>
2025-01-21 13:59:45 +0100

    Libft Finished

---
+++ libft/ft_calloc.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: franmore <franmore@student.42madrid.com>>  #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-01-14 09:12:23 by franmore          #+#    #+#             */
+/*   Updated: 2025-01-14 09:12:23 by franmore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+/// @brief This function allocates space for an array of "num" elements of a 
+///given "size" and initializes each bit to 0.
+/// @param num The number of elements.
+/// @param size The size of each element.
+/// @return Returns the matrix filled with 0.
+void	*ft_calloc(size_t num, size_t size)
+{
+	void	*matrix;
+
+	if (num == 0 || size == 0)
+		return (malloc(1));
+	matrix = malloc(num * size);
+	if (matrix == NULL)
+		return (NULL);
+	ft_memset(matrix, 0, num * size);
+	return (matrix);
+}
